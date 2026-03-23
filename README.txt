1536c907818152872a9ff97f304e5b45c3894f5c
Egal432 <stephan.kreidl@outlook.de>
2026-03-23 12:59:11 +0100

    v2

---
+++ README.txt
@@ -0,0 +1,1 @@
+Upload these files to GitHub Pages or another static host, then open the URL in Chrome on Android and install it.
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ygo-lp-tracker-v1";
+const CACHE_NAME = "ygo-lp-tracker-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,22 +7,20 @@ const ASSETS = [
   "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
