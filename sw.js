75e2f575a4f655c713c4c1846fabf0d91f8ddcda
Egal432 <stephan.kreidl@outlook.de>
2026-03-24 09:13:29 +0100

    v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ygo-lp-tracker-v3";
+const CACHE_NAME = "ygo-lp-tracker-v4";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
